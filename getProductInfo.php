1a9ad36e37016e5b7649d646409c35010d2b9c74
Harrison Oglesby <oglesby.harrison@gmail.com>
2016-04-13 03:37:20 +0000

    added get product info

---
+++ getProductInfo.php
@@ -0,0 +1,17 @@
+<?php
+
+include 'includes/database.php'
+
+$conn = getDatabaseConnection();
+
+if(isset($_GET['productId'])){
+   SELECT productName, productDescription
+    FROM oe_product WHERE productId = " . $_GET['productId'];
+	$records = getDataBySQL($sql);
+	foreach ($records as $record) {
+		echo "ProductName: " . $record['productName'] . "<br />";
+		echo "ProductDescription: " . $record['productDescription'] . "<br />";
+	}
+}
+
+?>
