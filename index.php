3fb0ef2691fa4594b03c28599d5f69e69cf65c63
Harrison Oglesby <oglesby.harrison@gmail.com>
2016-04-13 03:03:15 +0000

    starting beginning stuff

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>Team Project</title>
+		
+		<link href="./Lab 5_ Otter Express - Product Catalog_files/styles.css" rel="stylesheet">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
     </head>
     <body>
 
+
+
+        <footer>
+            <br>
+            
+            
+        </footer>
     </body>
 </html>
